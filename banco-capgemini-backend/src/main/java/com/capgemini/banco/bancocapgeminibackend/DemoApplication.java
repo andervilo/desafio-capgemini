d123c44ffae628622119f2969dda8adf94102a6e
Anderson Oliveira <anderson.oliveira@vibedesenv.com>
2020-02-05 14:18:57 -0300

    first commit

---
+++ banco-capgemini-backend/src/main/java/com/capgemini/banco/bancocapgeminibackend/DemoApplication.java
@@ -0,0 +1,13 @@
+package com.capgemini.banco.bancocapgeminibackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
